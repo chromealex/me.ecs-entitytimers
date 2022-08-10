b250a49b1035138533df1619bdc87e744990306f
Alex Silaev <chrome.alex@gmail.com>
2022-08-10 03:39:56 +0300

    World timers step update fix

--- Runtime/WorldInitializer.cs
+++ Runtime/WorldInitializer.cs
@@ -1,3 +1,11 @@
+#if FIXED_POINT_MATH
+using ME.ECS.Mathematics;
+using tfloat = sfloat;
+#else
+using Unity.Mathematics;
+using tfloat = System.Single;
+#endif
+
 namespace ME.ECS {
 
     public static class WorldInitializer {
@@ -9,6 +17,7 @@ namespace ME.ECS {
             WorldStaticCallbacks.RegisterCallbacks(Init, Dispose);
             WorldStaticCallbacks.RegisterCallbacks(InitState, DisposeState, InitResetState);
             WorldStaticCallbacks.RegisterCallbacks(OnEntityDestroy);
+            WorldStaticCallbacks.RegisterCallbacks(OnWorldLifetimeStep);
 
         }
 
@@ -17,6 +26,7 @@ namespace ME.ECS {
                 WorldStaticCallbacks.UnRegisterCallbacks(Init, Dispose);
                 WorldStaticCallbacks.UnRegisterCallbacks(InitState, DisposeState, InitResetState);
                 WorldStaticCallbacks.UnRegisterCallbacks(OnEntityDestroy);
+                WorldStaticCallbacks.UnRegisterCallbacks(OnWorldLifetimeStep);
             }
         }
         
@@ -45,6 +55,16 @@ namespace ME.ECS {
         public static void OnEntityDestroy(State state, in Entity entity) {
 
             state.pluginsStorage.Get<TimersStorage>(ref state.allocator, TimersStorage.key).OnEntityDestroy(ref state.allocator, in entity);
+
+        }
+
+        public static void OnWorldLifetimeStep(World world, ComponentLifetime step, tfloat deltaTime) {
+
+            if (step == ComponentLifetime.NotifyAllSystemsBelow) {
+
+                world.GetState().pluginsStorage.Get<TimersStorage>(ref world.GetState().allocator, TimersStorage.key).Update(ref world.GetState().allocator, deltaTime);
+
+            }
 
         }
 
